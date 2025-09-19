1dd9019c8c6fbfe880712194663705499bf24fd2
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-09-19 14:49:34 -0400

    complete

--- PolymorphismExercise/Program.cs
+++ PolymorphismExercise/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // without polymorphism
+
+            //SalariedEmployee salEmp = new SalariedEmployee(101, "John", 1000);
+            //Console.WriteLine(salEmp);
+            //Console.WriteLine($"\nEarnings = {salEmp.Earnings():C}\n\n");
+
+            //HourlyEmployee hrEmp = new HourlyEmployee(102, "Anne", 50, 25);
+            //Console.WriteLine(hrEmp);
+            //Console.WriteLine($"\nEarnings = {hrEmp.Earnings():C}\n\n");
+
+            //CommissionEmployee commEmp = new CommissionEmployee(103, "Mark", 10000, 0.12);
+            //Console.WriteLine(commEmp);
+            //Console.WriteLine($"\nEarnings = {commEmp.Earnings():C}\n\n");
+
+
+            // with polymorphism
+
+            //Employee emp;
+
+            //emp = new SalariedEmployee(101, "John", 1000);
+            //Console.WriteLine(emp);
+            //Console.WriteLine($"\nEarnings = {emp.Earnings():C}\n\n");
+
+            //emp = new HourlyEmployee(102, "Anne", 50, 25);
+            //Console.WriteLine(emp);
+            //Console.WriteLine($"\nEarnings = {emp.Earnings():C}\n\n");
+
+            //emp = new CommissionEmployee(103, "Mark", 10000, 0.12);
+            //Console.WriteLine(emp);
+            //Console.WriteLine($"\nEarnings = {emp.Earnings():C}\n\n");
+
+
+            // collection of objects
+
+            Employee[] employees = new Employee[3];
+            employees[0] = new SalariedEmployee(101, "John", 1000);
+            employees[1] = new HourlyEmployee(102, "Anne", 50, 25);
+            employees[2] = new CommissionEmployee(103, "Mark", 10000, 0.12);
+
+            foreach (Employee em in employees)
+            {
+                Console.WriteLine(em);
+                Console.WriteLine($"\nEarnings = {em.Earnings():C}\n\n");
+            }
         }
     }
 }
