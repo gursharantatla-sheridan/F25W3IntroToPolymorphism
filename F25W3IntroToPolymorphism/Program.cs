281edf5b4ed66201165514f2f5cbb9a6e38d1a63
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-09-19 13:37:41 -0400

    static polym

--- F25W3IntroToPolymorphism/Program.cs
+++ F25W3IntroToPolymorphism/Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Add(2, 3);
+
+            int[] arr = { 1, 2, 3 };
+            //PrintArray(arr);
+            //PrintArray(2, 3, 4, 5, 6, 6, 5, 43);
+
+            PrintArray(3, 3, 4, 45, 4, 3);
+        }
+
+        static void PrintArray(int[] n)
+        {
+
+        }
+
+        static void PrintArray(params int[] array)
+        {
+
+        }
+
+        static void Add(int n1, int n2)
+        {
+
+        }
+
+        //static int Add(int n1, int n2)
+        //{
+        //    return 1;
+        //}
+
+        static void Add(decimal n1, decimal n2)
+        {
+
+        }
+
+        static void Add(int n1, int n2, int n3)
+        {
+
+        }
+
+        static void Add(int n1, int n2, ref int n3)
+        {
+
         }
     }
 }
